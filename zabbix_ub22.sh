fe37dd422046d10ce8396538b9dab47f2eafdee2
Maksim <84027393+MrakoMaks@users.noreply.github.com>
2023-05-03 21:18:03 +0300

    Create zabbix_ub22.sh

---
+++ zabbix_ub22.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -e -p "Введите IP или домен Zabbix: " ZABBIX
+read -e -p "Введите имя этого узла в Zabbix: " SERVERNAME
+wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest%2Bubuntu20.04_all.deb
+zabbix-release_latest+ubuntu20.04_all.deb
+apt update
+apt install zabbix-agent2
+systemctl enable zabbix-agent2 --now
+sed -i "s/Server=127.0.0.1/Server=$ZABBIX/g" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=$ZABBIX/g" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/# Timeout=3/Timeout=30/g" /etc/zabbix/zabbix_agent2.conf
+sed -i "s/Hostname=Zabbix server/Hostname=$SERVERNAME/g" /etc/zabbix/zabbix_agent2.conf
+systemctl restart zabbix-agent2.service
+rm zabbix*deb
